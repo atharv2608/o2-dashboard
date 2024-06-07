ecca82bf4b67521152fa62b33e86087272bfef6e
Atharv Lingayat <atharvofficial7@gmail.com>
2024-06-07 18:53:37 +0530

    User name in dashboard configured

---
+++ src/components/dashboardComponents/DataBank.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DataBank() {
+  return (
+   <div className=''>
+     <div className='text-4xl text-white font-bold'>Data Bank</div>
+   </div>
+  )
+}
+
+export default DataBank
